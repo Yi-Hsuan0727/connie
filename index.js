e58899c3c97e90cf5126e8bf0c0002d91e2f642a
Yi-Hsuan0727 <a3154129@gmail.com>
2022-09-21 15:31:29 +0800

    push

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 var form = document.getElementById('sheetdb-form');
-        form.addEventListener("submit", e => {
-          e.preventDefault();
-          fetch(form.action, {
-              method : "POST",
-              body: new FormData(document.getElementById("sheetdb-form")),
-          }).then(
-              response => response.json()
-          ).then((html) => {
-            // you can put any JS code heres
-            // window.open('page2.html', '_blank');
-            window.alert("表單已送出！敬請靜待回覆。");
-            document.getElementById("sheetdb-form").reset();
-          });
-        });
-$("#sidebarButton").click(function(){
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  fetch(form.action, {
+    method: "POST",
+    body: new FormData(document.getElementById("sheetdb-form")),
+  }).then(
+    response => response.json()
+  ).then((html) => {
+    // you can put any JS code heres
+    // window.open('page2.html', '_blank');
+    window.alert("表單已送出！敬請靜待回覆。");
+    document.getElementById("sheetdb-form").reset();
+  });
+});
+$("#sidebarButton").click(function () {
   $("#sidebarButton").toggleClass("transform");
   $("#sidebar").slideToggle();
-}) ;
+});
